56e72d0b1a341e293ebb7387488c027ec5e58677
Ilya Kantor <iliakan@gmail.com>
2015-08-27 09:33:13 +0300

    rewritten for mongoose 4.1.x

--- index.js
+++ index.js
@@ -6,13 +6,21 @@ var mongoose = require('mongoose');
 module.exports = function patchMPromise(ns) {
 
   if (typeof ns.bind !== 'function') {
-    throw new TypeError("must include namespace to patch Mongoose Mpromise against");
+    throw new TypeError("must include namespace to patch Mongoose against");
   }
 
-  shimmer.wrap(mongoose.Mongoose.prototype.Promise.prototype, 'on', function (original) {
-    return function(event, callback) {
-      callback = ns.bind(callback);
-      return original.call(this, event, callback);
+  shimmer.wrap(mongoose.Mongoose.prototype.Query.prototype, 'exec', function (original) {
+    return function(op, callback) {
+      if (typeof op == 'function') op = ns.bind(op);
+      if (typeof callback == 'function') callback = ns.bind(callback);
+      return original.call(this, op, callback);
+    };
+  });
+
+  shimmer.wrap(mongoose.Mongoose.prototype.Query.base, '_wrapCallback', function (original) {
+    return function(method, callback, queryInfo) {
+      if (typeof callback == 'function') callback = ns.bind(callback);
+      return original.call(this, method, callback, queryInfo);
     };
   });
 };
